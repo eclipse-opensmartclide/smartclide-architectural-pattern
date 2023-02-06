959e86b8b0fb98ce966581949b9812d65d6474ae
Feryal Fulya Horozal <horozal@atb-bremen.de>
2023-02-06 01:24:20 +0100

    pattern application controller updated

--- src/main/java/org/eclipse/opensmartclide/architecturalpatterns/application/PatternApplicationController.java
+++ src/main/java/org/eclipse/opensmartclide/architecturalpatterns/application/PatternApplicationController.java
@@ -1,8 +1,12 @@
 package org.eclipse.opensmartclide.architecturalpatterns.application;
 
 import org.eclipse.opensmartclide.architecturalpatterns.service.ArchitecturalPatternsJsonHandler;
+import org.eclipse.opensmartclide.architecturalpatterns.supportedpatterns.ArchitecturalPatterns;
+
+import java.lang.IllegalArgumentException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
@@ -25,32 +30,41 @@ public class PatternApplicationController {
 
 	@PostMapping(value = "/application", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String applyPattern(@RequestParam("technology") String techName, @RequestParam("pattern") String pattern,
-			@Nullable @RequestParam("name") String projName, 
-			@Nullable @RequestParam("visibility") String visibility,
+			@Nullable @RequestParam("name") String projName, @Nullable @RequestParam("visibility") String visibility,
 			@RequestHeader String gitLabServerURL, @RequestHeader String gitlabToken) {
-		
-		String repoUrl = getProjectURL(techName, pattern);
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("gitLabServerURL", gitLabServerURL);
-		headers.set("gitlabToken", gitlabToken);
+		try {
+			String repoUrl = getProjectURL(techName, pattern);
+			System.out.println(repoUrl);
 
-		String query = "repoUrl=" + repoUrl + "&name=" + projName + "&visibility=" + visibility;
-		HttpEntity<String> request = new HttpEntity<>(query, headers);
+			if (repoUrl == null) {
+				throw new NullPointerException("Repository URL is not found.");
+			} else {
+				HttpHeaders headers = new HttpHeaders();
+				headers.set("gitLabServerURL", gitLabServerURL);
+				headers.set("gitlabToken", gitlabToken);
 
-		RestTemplate restTemplate = new RestTemplate();
-		String result = restTemplate.postForObject(importProjectURL, request, String.class);
+				String query = "repoUrl=" + repoUrl + "&name=" + projName + "&visibility=" + visibility;
+				HttpEntity<String> request = new HttpEntity<>(query, headers);
 
-		return result;
+				RestTemplate restTemplate = new RestTemplate();
+				String result = restTemplate.postForObject(importProjectURL, request, String.class);
+				return result;
+			}
+		} catch (IllegalArgumentException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Expected a git-based URL", e);
+		}
 	}
-	
-	public String getProjectURL(String techName, String pattern) {
-        final JsonNode jsonNode = projectJsonHandler.getProjectUrlsNode();
 
-            if (jsonNode.get(techName) == null) {
-                throw new IllegalArgumentException("Invalid technology received: " + techName + "is not a valid technology.");
-            }
-            return jsonNode.get(techName).asText();
+	public String getProjectURL(String techName, String pattern) {
+		final JsonNode projUrlsJsonNode = projectJsonHandler.getProjectUrlsNode();
+
+		if (projUrlsJsonNode.get(techName) == null) {
+			throw new IllegalArgumentException(
+					"Invalid technology received: " + techName + "is not a valid technology.");
+		}
+		
+		return 	projUrlsJsonNode.get(techName).get(pattern).asText();
 	}
 
 }
