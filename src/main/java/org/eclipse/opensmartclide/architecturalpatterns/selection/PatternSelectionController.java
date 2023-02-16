7fde50b154b5b53e79795adb0276fdc7dfc5b6ed
horozal <36187605+horozal@users.noreply.github.com>
2023-02-16 14:14:08 +0100

    return pattern values as percentages (#18)
    * return pattern values as percentages
    
    * remove unnecessary variable & update framework name

--- src/main/java/org/eclipse/opensmartclide/architecturalpatterns/selection/PatternSelectionController.java
+++ src/main/java/org/eclipse/opensmartclide/architecturalpatterns/selection/PatternSelectionController.java
@@ -13,52 +13,50 @@ import java.util.Map;
 
 @RestController
 public class PatternSelectionController {
-    private final ArchitecturalPatternsJsonHandler surveyJsonHandler;
+	private final ArchitecturalPatternsJsonHandler surveyJsonHandler;
 
-    public PatternSelectionController(final ArchitecturalPatternsJsonHandler surveyJsonHandler) {
-        this.surveyJsonHandler = surveyJsonHandler;
-    }
+	public PatternSelectionController(final ArchitecturalPatternsJsonHandler surveyJsonHandler) {
+		this.surveyJsonHandler = surveyJsonHandler;
+	}
 
-    @PostMapping(
-            value = "/evaluation",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public Map<ArchitecturalPatterns, Integer> evaluateSurveyInput(@RequestBody List<String> input) {
-        return calculatePatternValues(input);
-    }
+	@PostMapping(value = "/evaluation", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<ArchitecturalPatterns, Integer> evaluateSurveyInput(@RequestBody List<String> input) {
+		return calculatePatternValues(input);
+	}
 
-    private HashMap<ArchitecturalPatterns, Integer> calculatePatternValues(final List<String> input) {
-        // Initialize
-        final JsonNode surveyEvaluationNode = surveyJsonHandler.getSurveyEvaluationNode();
-        final HashMap<ArchitecturalPatterns, Integer> patternValues = initializePatternValues();
+	private HashMap<ArchitecturalPatterns, Integer> calculatePatternValues(final List<String> input) {
+		// Initialize
+		final JsonNode surveyEvaluationNode = surveyJsonHandler.getSurveyEvaluationNode();
+		final HashMap<ArchitecturalPatterns, Integer> patternValues = initializePatternValues();
+		int totalValue = 0;
 
-        // Iterate over survey question IDs
-        for (String id : input) {
-            if (surveyEvaluationNode.get(id) == null) {
-                throw new IllegalArgumentException("Invalid survey input received: " + id + "is not a valid question ID.");
-            }
-            JsonNode valuesJsonNode = surveyEvaluationNode.get(id);
+		// Iterate over survey question IDs
+		for (String id : input) {
+			if (surveyEvaluationNode.get(id) == null) {
+				throw new IllegalArgumentException(
+						"Invalid survey input received: " + id + "is not a valid question ID.");
+			}
+			JsonNode valuesJsonNode = surveyEvaluationNode.get(id);
 
-            for (ArchitecturalPatterns pattern : ArchitecturalPatterns.values()) {
-                int newValue = patternValues.get(pattern) + valuesJsonNode.get(pattern.name()).asInt();
-                // Update evaluation score for pattern
-                patternValues.put(pattern, newValue);
-            }
-        }
-        return patternValues;
-    }
+			for (ArchitecturalPatterns pattern : ArchitecturalPatterns.values()) {
+				int value = valuesJsonNode.get(pattern.name()).asInt();
+				int newValue = patternValues.get(pattern) + value;
+				// Update evaluation score for pattern
+				patternValues.put(pattern, newValue);
+				totalValue = totalValue + value;
+			}
+		}
 
-    private HashMap<ArchitecturalPatterns, Integer> initializePatternValues() {
-        return new HashMap<>(
-                Map.of(
-                        ArchitecturalPatterns.LAYERED, 0,
-                        ArchitecturalPatterns.EVENT_DRIVEN, 0,
-                        ArchitecturalPatterns.MICROKERNEL, 0,
-                        ArchitecturalPatterns.MICROSERVICES, 0,
-                        ArchitecturalPatterns.SERVICE_ORIENTED, 0,
-                        ArchitecturalPatterns.SPACE_BASED, 0
-                )
-        );
-    }
+		for (Map.Entry<ArchitecturalPatterns, Integer> entry : patternValues.entrySet()) {
+			double percentage = (entry.getValue() * 100) / (double) totalValue;
+			patternValues.put(entry.getKey(), (int) percentage);
+		}
+		return patternValues;
+	}
+
+	private HashMap<ArchitecturalPatterns, Integer> initializePatternValues() {
+		return new HashMap<>(Map.of(ArchitecturalPatterns.LAYERED, 0, ArchitecturalPatterns.EVENT_DRIVEN, 0,
+				ArchitecturalPatterns.MICROKERNEL, 0, ArchitecturalPatterns.MICROSERVICES, 0,
+				ArchitecturalPatterns.SERVICE_ORIENTED, 0, ArchitecturalPatterns.SPACE_BASED, 0));
+	}
 }
